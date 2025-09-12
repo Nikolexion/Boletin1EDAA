9558f6d3b345c8bc478e46dfafa0a346e0cb2a4f
Nikolexion <nfalc2003@gmail.com>
2025-09-12 15:08:31 -0300

    feat: separadas las dos pruebas en dos archivos distintos

--- sequential.cpp
+++ sequential.cpp
@@ -2,12 +2,11 @@
 #include <vector>
 
 template<typename T>
-void sequential_search(const std::vector<T>& data, T target) {
+int sequential_search(const std::vector<T>& data, T target) {
     for (auto& item : data) {
         if (item == target) {
-            std::cout << "Encontrado: " << target << std::endl;
-            return;
+            return &item - &data[0];
         }
     }
-    std::cout << "No encontrado: " << target << std::endl;
+    return -1;
 }
